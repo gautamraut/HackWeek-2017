82932c90b1edfd3b5eb734e28e9bdf492259137d
Shrasti Bansal <shrbansa@adobe.com>
2017-08-22 15:31:57 +0530

    more changes for list

--- app/src/main/java/com/example/shrbansa/listtrial/MainActivity.java
+++ app/src/main/java/com/example/shrbansa/listtrial/MainActivity.java
@@ -6,28 +6,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import com.example.shrbansa.listtrial.model.Message;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MessageAdapter.MessageItemSelector{
+
 	private List<Message> movieList = new ArrayList<>();
 	private RecyclerView recyclerView;
+
 	private MessageAdapter mAdapter;
+	private EditText finalMessageView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		finalMessageView = (EditText)findViewById(R.id.finalText);
+		finalMessageView.setMovementMethod(new ScrollingMovementMethod());
+
 		recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
-		mAdapter = new MessageAdapter(movieList);
+		mAdapter = new MessageAdapter(movieList, this);
 		RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
 		recyclerView.setLayoutManager(mLayoutManager);
 		recyclerView.setItemAnimator(new DefaultItemAnimator());
@@ -37,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 	}
 
 	private void prepareMovieData() {
-		Message movie = new Message("Mad Max: Fury Road", "Action & Adventure", "2015");
+		Message movie = new Message("Mad Max: Fury RoadThis is testdata abd a lot of data bla bla blaMad Max: Fury RoadThis is testdata abd a lot of data bla bla blaMad Max: Fury RoadThis is testdata abd a lot of data bla bla blaMad Max: Fury RoadThis is testdata abd a lot of data bla bla bla", "Action & Adventure", "2015");
 		movieList.add(movie);
 
 		movie = new Message("Inside Out", "Animation, Kids & Family", "2015");
@@ -108,6 +116,23 @@ public class MainActivity extends AppCompatActivity {
 
 		movie = new Message("Guardians of the Galaxy", "Science Fiction & Fantasy", "2014");
 		movieList.add(movie);
+		movie = new Message("Aliens", "Science Fiction", "1986");
+		movieList.add(movie);
+
+		movie = new Message("Chicken Run", "Animation", "2000");
+		movieList.add(movie);
+
+		movie = new Message("Back to the Future", "Science Fiction", "1985");
+		movieList.add(movie);
+
+		movie = new Message("Raiders of the Lost Ark", "Action & Adventure", "1981");
+		movieList.add(movie);
+
+		movie = new Message("Goldfinger", "Action & Adventure", "1965");
+		movieList.add(movie);
+
+		movie = new Message("Guardians of the Galaxy", "Science Fiction & Fantasy", "2014");
+		movieList.add(movie);
 
 		mAdapter.notifyDataSetChanged();
 	}
@@ -126,6 +151,8 @@ public class MainActivity extends AppCompatActivity {
 			case R.id.test_api:
 				startRestTestActivity();
 				return true;
+			case R.id.add_newItem:
+				handleItemAddition("Mad Max: Fury RoadThis is testdata abd a lot of data bla bla blaMad Max: Fury RoadThis is test");
 			default:
 				return super.onOptionsItemSelected(item);
 		}
@@ -135,5 +162,22 @@ public class MainActivity extends AppCompatActivity {
 	{
 		Intent intent = new Intent(this, RestTester.class);
 		startActivity(intent);
+	}
+
+
+	private void handleItemAddition(String message){
+		Message movie = new Message(message, "Action & Adventure", "2015");
+		movieList.add(movie);
+		mAdapter.notifyDataSetChanged();
+	}
+
+	@Override
+	public void MessageItemSelected(String message, boolean isAccepted, int position)
+	{
+		movieList.remove(position);
+		mAdapter.notifyDataSetChanged();
+		if(isAccepted) {
+			finalMessageView.append(message);
+		}
 	}
 }
--- app/src/main/java/com/example/shrbansa/listtrial/MessageAdapter.java
+++ app/src/main/java/com/example/shrbansa/listtrial/MessageAdapter.java
@@ -5,9 +5,11 @@ package com.example.shrbansa.listtrial;
  */
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.shrbansa.listtrial.model.Message;
@@ -17,20 +19,23 @@ import java.util.List;
 public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MyViewHolder> {
 
 	private List<Message> moviesList;
+	private MessageItemSelector mListener;
 
 	public class MyViewHolder extends RecyclerView.ViewHolder {
 		public TextView title, year, genre;
+		public ImageButton accept, reject;
 
 		public MyViewHolder(View view) {
 			super(view);
 			title = (TextView) view.findViewById(R.id.title);
-//			genre = (TextView) view.findViewById(R.id.genre);
-//			year = (TextView) view.findViewById(R.id.year);
+			accept = (ImageButton)view.findViewById(R.id.accept);
+			reject = (ImageButton)view.findViewById(R.id.reject);
 		}
 	}
 
 
-	public MessageAdapter (List<Message> moviesList) {
+	public MessageAdapter (List<Message> moviesList, MessageItemSelector listener) {
+		this.mListener = listener;
 		this.moviesList = moviesList;
 	}
 
@@ -44,15 +49,36 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MyViewHo
 
 
 	@Override
-	public void onBindViewHolder(MyViewHolder holder, int position) {
+	public void onBindViewHolder(MyViewHolder holder, final int position) {
 		Message msg = moviesList.get(position);
 		holder.title.setText(msg.getMessage());
-//		holder.genre.setText(movie.getGenre());
-//		holder.year.setText(movie.getYear());
+		holder.accept.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View view)
+			{
+				Log.d("item accepted" , String.valueOf(position));
+				mListener.MessageItemSelected(moviesList.get(position).getMessage(), true, position);
+			}
+		});
+		holder.reject.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View view)
+			{
+				Log.d("item rejecetd" , String.valueOf(position));
+				mListener.MessageItemSelected(moviesList.get(position).getMessage(), false, position);
+			}
+		});
 	}
 
 	@Override
 	public int getItemCount() {
 		return moviesList.size();
+	}
+
+	public interface MessageItemSelector
+	{
+		void MessageItemSelected(String message, boolean isAccepted,  int position);
 	}
 }
